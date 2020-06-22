e89a3feac4a738bfe3acacd18ec09afd49829f22
Devon Ward <devoncward@gmail.com>
2020-06-22 16:16:59 -0600

    Fixed ugly for loop :)

--- Main.cpp
+++ Main.cpp
@@ -2,11 +2,15 @@
 
 int main() {
     std::cout << "Hello World!" << std::endl;
-
+    /*
     for (int i{ 0 }; i < 15; ++i) {
         std::cout << "hi ";
     }
     std::cout << '\n';
-
+    */
+   for(int i = 0; i < 15; i++) {
+       std::cout << "hi" ;
+   }
+   std::cout << std::endl;
     return 0;
 }
